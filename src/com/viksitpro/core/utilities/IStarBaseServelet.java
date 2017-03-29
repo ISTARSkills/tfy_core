b898ebd542927880454fe85140c4f8e79c363061
sumanth-istar <sumanth@istarindia.com>
2017-03-29 14:25:11 +0530

    Signed-off-by: sumanth-istar <sumanth@istarindia.com>

---
+++ src/com/viksitpro/core/utilities/IStarBaseServelet.java
@@ -0,0 +1,59 @@
+/**
+ * 
+ */
+package com.viksitpro.core.utilities;
+
+import java.util.Enumeration;
+import java.util.HashMap;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @vauthor Vaibhav
+ *
+ */
+public class IStarBaseServelet extends HttpServlet {
+	
+	private static final long serialVersionUID = 8222264765449309590L;
+
+	public void printParams(HttpServletRequest request) {
+		com.viksitpro.core.utilities.UUIUtils.printlog("From IStarBaseServelet:");
+		for (Object key : request.getParameterMap().keySet()) {
+			String paramName = key.toString();
+			Object paramValue = request.getParameter(key.toString());
+			com.viksitpro.core.utilities.UUIUtils.printlog("Param -> " + paramName + " : Value ->" + paramValue);
+		}
+
+	}
+
+	public void printAttrs(HttpServletRequest request) {
+		com.viksitpro.core.utilities.UUIUtils.printlog("From IStarBaseServelet: ");
+		for (Enumeration<String> enumeration = request.getAttributeNames(); enumeration.hasMoreElements();) {
+			String attributeName = enumeration.nextElement();
+			Object attribute = request.getAttribute(attributeName);
+			com.viksitpro.core.utilities.UUIUtils.printlog("Attr -> " + attributeName + " : Value ->" + attribute);
+		}
+
+	}
+	
+	public void printAll(HttpServletRequest request) {
+		printParams(request);
+		printAttrs(request);
+	}
+
+	public HashMap<String, String> getReqMap(HttpServletRequest request) {
+		HashMap<String, String> data = new HashMap<>();
+		for (String iterable_element : request.getParameterMap().keySet()) {
+			data.put(iterable_element, request.getParameter(iterable_element));
+		}
+		
+		Enumeration<String> attrs = request.getAttributeNames();
+		while (attrs.hasMoreElements()) {
+			String name = (String) attrs.nextElement();
+			String value = "" + request.getAttribute(name);
+			data.put(name, value);
+		}
+		return data;
+	}
+}
---
+++ src/com/viksitpro/core/utilities/UUIUtils.java
@@ -0,0 +1,39 @@
+/**
+ * 
+ */
+package com.viksitpro.core.utilities;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.UUID;
+import org.apache.log4j.Logger;
+
+
+
+/**
+ * @author vaibhav
+ *
+ */
+public class UUIUtils {
+	static Logger log = Logger.getLogger(UUIUtils.class.getName());
+
+	public String getUUID() {
+		return UUID.randomUUID().toString();
+	}
+
+	public static void printlog(Object message) {
+		SimpleDateFormat format = new SimpleDateFormat("dd MMM yyyy HH:mm:ss.SSS");
+		Date date = new Date();
+		String timeStampString = format.format(date);
+		log.error(timeStampString + " " + message.toString());
+		System.err.println(message.toString());
+	}
+
+	public static void printlog(Object cname, Object message) {
+		SimpleDateFormat format = new SimpleDateFormat("dd MMM yyyy HH:mm:ss.SSS");
+		Date date = new Date();
+		String timeStampString = format.format(date);
+		log.error(timeStampString + " - " + cname + " - " + message.toString());
+		System.err.println(message.toString());
+	}
+}
