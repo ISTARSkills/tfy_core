fe7c557697fe88c9a513de84b962bd2c7ca568a6
ravybathla <ravitashaw@istarindia.com>
2017-04-07 16:27:13 +0530

    IstarUser: login_type and is_verified

--- test/istarcore/db_updates.sql
+++ test/istarcore/db_updates.sql
@@ -1,1 +1,7 @@
+ALTER TABLE "public"."istar_user"
+ADD COLUMN "is_verified" bool;
 
+ALTER TABLE "public"."istar_user"
+ADD COLUMN "type" varchar(255);
+
+ALTER TABLE "public"."istar_user" RENAME "type" TO "login_type";
