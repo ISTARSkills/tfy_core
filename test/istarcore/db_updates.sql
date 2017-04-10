0c989f26154813323c3154d789bdd7852d9664c5
ravybathla <ravitashaw@istarindia.com>
2017-04-10 09:34:30 +0530

    Update db_updates.sql

--- test/istarcore/db_updates.sql
+++ test/istarcore/db_updates.sql
@@ -5,3 +5,6 @@ ALTER TABLE "public"."istar_user"
 ADD COLUMN "type" varchar(255);
 
 ALTER TABLE "public"."istar_user" RENAME "type" TO "login_type";
+
+ALTER TABLE "public"."presentation"
+ADD COLUMN "is_deleted" bool;
