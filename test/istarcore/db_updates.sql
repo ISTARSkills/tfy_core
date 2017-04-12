81d8c08a1635e5a0dfe39e6eb996182a2a5e2bfe
ravybathla <ravitashaw@istarindia.com>
2017-04-12 12:06:31 +0530

    UserGamification
    for Points and Coins

--- test/istarcore/db_updates.sql
+++ test/istarcore/db_updates.sql
@@ -6,5 +6,19 @@ ADD COLUMN "type" varchar(255);
 
 ALTER TABLE "public"."istar_user" RENAME "type" TO "login_type";
 
-ALTER TABLE "public"."presentation"
-ADD COLUMN "is_deleted" bool;
+CREATE TABLE "public"."user_gamification" (
+"id" int4 NOT NULL,
+"istar_user" int4,
+"skill_objective" int4,
+"points" int4,
+"coins" int4,
+"created_at" timestamp,
+"updated_at" timestamp,
+PRIMARY KEY ("id")
+)
+WITH (OIDS=FALSE)
+;
+
+ALTER TABLE "public"."user_gamification"
+ADD FOREIGN KEY ("istar_user") REFERENCES "public"."istar_user" ("id") ON DELETE NO ACTION ON UPDATE NO ACTION,
+ADD FOREIGN KEY ("skill_objective") REFERENCES "public"."skill_objective" ("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
