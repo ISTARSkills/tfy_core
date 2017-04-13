fe23a6fb3e57400a7ac4c0c4ba700e0b4798f4a3
ravitashaw <ravitashaw@istarindia.com>
2017-04-13 11:09:16 +0530

    order by orderId in Getting questions for assessment

--- src/com/viksitpro/core/dao/entities/Assessment.java
+++ src/com/viksitpro/core/dao/entities/Assessment.java
@@ -14,6 +14,7 @@ import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.OrderBy;
 
 /**
  * Assessment entity. @author MyEclipse Persistence Tools
@@ -158,7 +159,7 @@ public class Assessment implements java.io.Serializable {
 	}
 
 	@OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY, mappedBy = "assessment")
-
+	@OrderBy(clause = "orderId")
 	public Set<AssessmentQuestion> getAssessmentQuestions() {
 		return this.assessmentQuestions;
 	}
