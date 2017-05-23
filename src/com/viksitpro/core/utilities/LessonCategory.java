42acd5fa343d62a8bb44f3e2c8b11d1b81aa90b2
ipconfig <ipconfig>
2017-05-23 20:37:57 +0530

    added lessonCategory constants

---
+++ src/com/viksitpro/core/utilities/LessonCategory.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package com.viksitpro.core.utilities;
+
+/**
+ * @author mayank
+ *
+ */
+public class LessonCategory {
+
+	public static final String ILT ="ILT";
+	public static final String ELT ="ELT";
+
+	public static final String BOTH ="BOTH";
+}
