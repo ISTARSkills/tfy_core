e30da93feeeb940a63a3751888c8c673bc5a6e21
ipconfig <ipconfig>
2017-05-30 17:19:43 +0530

    added task item category

--- src/com/viksitpro/core/utilities/TaskItemCategory.java
+++ src/com/viksitpro/core/utilities/TaskItemCategory.java
@@ -28,6 +28,7 @@ public class TaskItemCategory {
 	public static final String GAME = "GAME";
 	public static final String VIDEO = "VIDEO";
 	public static final String CLASSROOM_ASSESSMENT = "CLASSROOM_ASSESSMENT";
+	public static final String CLASSROOM_SESSION_STUDENT = "CLASSROOM_SESSION_STUDENT";
 	
 	
 }
