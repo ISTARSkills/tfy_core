c92d953d71ba45157e1131ff508d18b657bb423d
ipconfig <ipconfig>
2017-06-21 23:20:34 +0530

    testable

---
+++ src/com/viksitpro/core/utilities/TrainerEmpanelmentStageTypes.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package com.viksitpro.core.utilities;
+
+/**
+ * @author mayank
+ *
+ */
+public class TrainerEmpanelmentStageTypes {
+
+	public final static String SIGNED_UP="SIGNED_UP";
+	public final static String ASSESSMENT_DONE="ASSESSMENT_DONE";
+	public final static String SME_INTERVIEW="SME_INTERVIEW";
+	public final static String DEMO="DEMO";	
+	
+}
---
+++ src/com/viksitpro/core/utilities/TrainerEmpanelmentStatusTypes.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package com.viksitpro.core.utilities;
+
+/**
+ * @author mayank
+ *
+ */
+public class TrainerEmpanelmentStatusTypes {
+
+	public static final String SELECTED="SELECTED";
+	public static final String REJECTED="REJECTED";
+	
+}
