dbfbf2752de490570047060a00c2cd0c91462640
ipconfig <ipconfig>
2017-06-23 18:47:44 +0530

    telephonic

--- src/com/viksitpro/core/utilities/TrainerEmpanelmentStageTypes.java
+++ src/com/viksitpro/core/utilities/TrainerEmpanelmentStageTypes.java
@@ -9,9 +9,11 @@ package com.viksitpro.core.utilities;
  */
 public class TrainerEmpanelmentStageTypes {
 
-	public final static String SIGNED_UP="SIGNED_UP";
-	public final static String ASSESSMENT_DONE="ASSESSMENT_DONE";
-	public final static String SME_INTERVIEW="SME_INTERVIEW";
-	public final static String DEMO="DEMO";	
+	public final static String TELEPHONIC_INTERVIEW="L1";
+	public final static String SIGNED_UP="L2";
+	public final static String ASSESSMENT_DONE="L3";
+	public final static String SME_INTERVIEW="L4";
+	public final static String DEMO="L5";
+	public final static String FITMENT_INTERVIEW ="L6";
 	
 }
--- src/com/viksitpro/core/utilities/TrainerEmpanelmentStatusTypes.java
+++ src/com/viksitpro/core/utilities/TrainerEmpanelmentStatusTypes.java
@@ -11,5 +11,6 @@ public class TrainerEmpanelmentStatusTypes {
 
 	public static final String SELECTED="SELECTED";
 	public static final String REJECTED="REJECTED";
+	public static final String NOT_REACHED ="NOT_REACHED";
 	
 }
