cf6e01e9d72237575c3897b0a4ab152799328cca
mayank@istarindia.com <ISTAR-SERVER-PU-1@Main-Server>
2017-07-06 13:30:47 +0530

    helper added

--- src/com/viksitpro/core/utilities/AppProperies.java
+++ src/com/viksitpro/core/utilities/AppProperies.java
@@ -5,7 +5,11 @@ package com.viksitpro.core.utilities;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Properties;
+
+
 
 /**
  * @author ISTAR-SERVER-PU-1
@@ -24,11 +28,27 @@ public class AppProperies {
 			e.printStackTrace();
 
 		}
+		
+		// read XML
 	}
 	
 	public static String getProperty(String property) {
 		String deploymentType = "prod";
 		deploymentType = properties.getProperty(property);
 		return deploymentType;
+	}
+	
+	public static  String generatePopOver(String elementName) {
+		try {
+			DBUTILS db = new DBUTILS();
+			HashMap<String, Object> item = db.executeQuery("select * from helper where element_name='"+elementName+"'").get(0);
+			
+			return "<i class='pull-right-margin-left fa fa-question-circle' aria-hidden='true' data-toggle='popover' "
+					+ " data-trigger='hover' title='"+item.get("header")+"' data-content='"+item.get("body")+"'  data-placement='"+item.get("position")+"'></i>";
+		} catch (Exception e) {
+			e.printStackTrace();
+			return "";
+		}
+		
 	}
 }
