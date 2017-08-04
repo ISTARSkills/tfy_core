12fb5ad142b43b325d7a431e9d823d4bf68d3881
vinayistar <vinay@istarindia.com>
2017-08-04 18:49:14 +0530

    validation added in nsdc form

--- src/com/viksitpro/core/utilities/CustomFormElementDataTypes.java
+++ src/com/viksitpro/core/utilities/CustomFormElementDataTypes.java
@@ -13,4 +13,5 @@ public class CustomFormElementDataTypes {
 	public static final  String NUMBER = "NUMBER";
 	public static final  String STRING = "STRING";
 	public static final  String DECIMAL = "DECIMAL";
+	public static final  String DATE = "DATE";
 }
