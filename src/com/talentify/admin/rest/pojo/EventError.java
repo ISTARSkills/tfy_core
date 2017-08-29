4d9efdd4d441a4f0f99f01334a9b436a42342527
ipconfig <ipconfig>
2017-08-29 18:15:52 +0530

    event card and event error pojo

---
+++ src/com/talentify/admin/rest/pojo/EventError.java
@@ -0,0 +1,33 @@
+/**
+ * 
+ */
+package com.talentify.admin.rest.pojo;
+
+/**
+ * @author mayank
+ *
+ */
+public class EventError {
+
+	String errorColor;
+	String errorValue;
+	
+	public String getErrorColor() {
+		return errorColor;
+	}
+	public void setErrorColor(String errorColor) {
+		this.errorColor = errorColor;
+	}
+	public String getErrorValue() {
+		return errorValue;
+	}
+	public void setErrorValue(String errorValue) {
+		this.errorValue = errorValue;
+	}
+	public EventError() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+}
